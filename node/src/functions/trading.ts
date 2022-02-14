3dd9ca6a8037116807298c58cf8b73c6c19e1bca
DeducMe <deducme@gmail.com>
2022-02-15 00:09:25 +0300

    initial

---
+++ node/src/functions/trading.ts
@@ -0,0 +1,75 @@
+(function () {
+  const axios = require("axios");
+  const { keyId, secretKey } = require("../config");
+
+  const setTrade = async (ticker: string, tradeWay: string, alpaca: any) => {
+    await alpaca.createOrder({
+      symbol: ticker,
+      notional: 1000, // will buy fractional shares
+      side: tradeWay,
+      type: "market",
+      time_in_force: "day",
+    });
+    console.log(`look mom i ${tradeWay} stonks: ${ticker}`);
+  };
+
+  function timeNow(minusHours?: number) {
+    const minusMiliSeconds = 60 * 60 * 1000 * (minusHours || 0);
+    const minus15Minutes = 60 * 1000 * 20;
+    const date = new Date();
+
+    return new Date(
+      date.getTime() - minusMiliSeconds - minus15Minutes
+    ).toISOString();
+  }
+
+  async function getHighestLowest(ticker: string, alpaca: any) {
+    const date1HourAgo = timeNow(70);
+    const dateNowTime = timeNow();
+    let resp = alpaca.getBarsV2(
+      ticker,
+      {
+        start: date1HourAgo,
+        end: dateNowTime,
+        timeframe: "15Min",
+        adjustment: "all",
+      },
+      alpaca.configuration
+    );
+
+    let lowest = 9999999999;
+    let highest = 0;
+    let responses = [];
+
+    for await (let bar of resp) {
+      responses.push(bar);
+      if (bar.LowPrice < lowest) {
+        lowest = bar.LowPrice;
+      }
+      if (bar.HighPrice > highest) {
+        highest = bar.HighPrice;
+      }
+    }
+
+    return {
+      lowest,
+      highest,
+      responses,
+    };
+  }
+
+  async function getLastTrade(ticker: string) {
+    return axios
+      .get(`https://data.alpaca.markets/v2/stocks/${ticker}/trades/latest`, {
+        headers: {
+          "APCA-API-KEY-ID": keyId,
+          "APCA-API-SECRET-KEY": secretKey,
+        },
+      })
+      .then((response: any) => {
+        return response.data.trade.p;
+      });
+  }
+
+  module.exports = { setTrade, timeNow, getHighestLowest, getLastTrade };
+})();
---
+++ node/src/index.ts
@@ -0,0 +1,134 @@
+(function () {
+  const {
+    setTrade,
+    getHighestLowest,
+    getLastTrade,
+  } = require("./functions/trading");
+
+  const Alpaca = require("@alpacahq/alpaca-trade-api");
+  const { keyId, secretKey } = require("./config");
+  const alpaca = new Alpaca({
+    keyId: keyId,
+    secretKey: secretKey,
+    paper: true,
+  });
+
+  const getAsset = async (ticker: string, stocksTimeOut: number) => {
+    let lastTrade;
+    let lastHighest = <number | null>null;
+    let lastLowest = <number | null>null;
+
+    let awaitForBuyBars = <boolean>false;
+    let awaitForSellBars = <boolean>false;
+
+    let farthestBarBuyTime = <number | null>null;
+    let latestBarBuyTime = <number | null>null;
+    let tradeLatestBuyBarTime = <number | null>null;
+
+    let farthestBarSellTime = <number | null>null;
+    let latestBarSellTime = <number | null>null;
+    let tradeLatestSellBarTime = <number | null>null;
+
+    setInterval(async () => {
+      const barsForBuy = await getHighestLowest(ticker, alpaca);
+      const barsForSell = JSON.parse(JSON.stringify(barsForBuy));
+      barsForBuy.length = 20;
+      barsForSell.length = 10;
+
+      lastTrade = await getLastTrade(ticker, alpaca);
+
+      farthestBarBuyTime = Date.parse(barsForBuy.responses[0].Timestamp);
+      latestBarBuyTime = Date.parse(
+        barsForBuy.responses[barsForBuy.responses.length - 1].Timestamp
+      );
+      if (farthestBarBuyTime === tradeLatestBuyBarTime && awaitForBuyBars) {
+        awaitForBuyBars = false;
+      }
+
+      farthestBarSellTime = Date.parse(barsForSell.responses[0].Timestamp);
+      latestBarSellTime = Date.parse(
+        barsForSell.responses[barsForSell.responses.length - 1].Timestamp
+      );
+      if (farthestBarSellTime === tradeLatestSellBarTime && awaitForSellBars) {
+        awaitForSellBars = false;
+      }
+
+      // farthestBarTime = barsForBuy.responses[0].Timestamp;
+
+      if (lastLowest && lastHighest) {
+        try {
+          if (lastLowest > lastTrade && !awaitForBuyBars) {
+            setTrade(ticker, "buy", alpaca);
+
+            awaitForBuyBars = true;
+            tradeLatestBuyBarTime = latestBarBuyTime;
+
+            console.log(lastLowest, lastHighest, " bought by ", lastTrade);
+          }
+          if (lastHighest < lastTrade && !awaitForSellBars) {
+            setTrade(ticker, "sell", alpaca);
+
+            awaitForSellBars = true;
+            tradeLatestSellBarTime = latestBarSellTime;
+
+            console.log(lastLowest, lastHighest, " sold by ", lastTrade);
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      }
+
+      lastHighest = barsForSell.highest;
+      lastLowest = barsForBuy.lowest;
+
+      // console.log(lastHighest, lastLowest, lastTrade, ticker);
+    }, stocksTimeOut);
+  };
+
+  const stocks = ["AAPL", "MSFT", "TSLA", "AMD", "BABA", "TAL", "COIN"];
+  const stocksWatchTimeouts = <any>[];
+  let working = true;
+
+  function startTrading() {
+    stocks.forEach((item, index) => {
+      stocksWatchTimeouts.push(
+        setTimeout(() => {
+          getAsset(item, stocks.length * 650);
+        }, 650 * index)
+      );
+    });
+  }
+
+  setInterval(() => {
+    const dateNow = new Date();
+    const finishDate = new Date();
+    const startDate = new Date();
+
+    const timeNow = dateNow.getTime();
+
+    finishDate.setHours(23, 55, 0);
+    startDate.setHours(17, 35, 0);
+
+    const startTime = startDate.getTime();
+    const finishTime = finishDate.getTime();
+    if (timeNow >= startTime && !working) {
+      working = true;
+      startTrading();
+
+      console.log("trading started");
+    }
+    if (timeNow >= finishTime && working) {
+      alpaca.closeAllPositions();
+      stopAllTasks();
+      working = false;
+
+      console.log("all tasks finished and positions closed");
+    }
+  }, 1000);
+
+  function stopAllTasks() {
+    stocksWatchTimeouts.map((item: any) => {
+      clearTimeout(item);
+    });
+  }
+})();
